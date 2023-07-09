8b390333918c646525a6a8ff9506e9f29fa0ffdb
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-07-09 13:24:29 +0200

    feat: Typeinator finished

--- projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
@@ -1,4 +1,19 @@
-// Put your announceMachines function here! ✨
-// See ./original.js for its older JavaScript code.
+function announceMachines(announce, ...args) {
+	let labelsCount = 0;
+
+	for (let machine of args) {
+		let label;
+		if (machine.label) {
+			label = machine.label;
+			labelsCount += 1;
+		} else {
+			label = `Make: ${machine.make}; Model: ${machine.model}`;
+		}
+
+		announce(label);
+	}
+
+	return labelsCount;
+}
 
 module.exports.announceMachines = announceMachines;
--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,5 +1,26 @@
 // Put your checkEmotion and speak functions here! ✨
 // See ./original.js for their older JavaScript code.
 
+async function checkEmotion(knownEmotions, emotion) {
+	// Simulate database processing time by waiting a second...
+	return new Promise((resolve, _) => {
+		setTimeout(() => {
+			resolve(knownEmotions.has(emotion));
+		}, 1000);
+	});
+}
+
+async function speak(knownEmotions, newEmotion, phrase) {
+	const hasEmotion = await checkEmotion(knownEmotions, newEmotion);
+
+	if (hasEmotion) {
+		return Promise.resolve(`"${phrase}" (${newEmotion})`);
+	} else {
+		return Promise.reject(
+			new Error(`Does not compute. I do not understand ${newEmotion}.`)
+		);
+	}
+}
+
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
